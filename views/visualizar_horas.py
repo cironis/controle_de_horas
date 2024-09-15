602b31b5474c4d1308928ad3973161c868392b93
cironis <ciroshia@gmail.com>
2024-09-15 19:35:24 +0000

    Updating app.py

---
+++ views/visualizar_horas.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+st.set_page_config(page_title="Visualizar horas", page_icon="🕒", layout="wide")
+
+@st.cache_data
+def load_main_dataframe(worksheet):
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read(worksheet=worksheet)
+    return df
+
+st.title("Database")
+
+df = load_main_dataframe("Horas por dia")
