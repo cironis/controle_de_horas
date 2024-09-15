14ca8a87d6b63357bf89a5c9b280c50b97f62126
cironis <ciroshia@gmail.com>
2024-09-15 17:06:48 +0000

    Updating app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# --- PAGE SETUP ---
+visualizar_horas = st.Page(
+    "views/visualizar_horas.py",
+    title="Visualizar horas",
+    icon=":material/overview:",
+)
+
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Visualização": [visualizar_horas],
+
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+
+# --- RUN NAVIGATION ---
+pg.run()
