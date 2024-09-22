143c40b9b3578dc012136ed3410cc4640ba46391
cironis <ciroshia@gmail.com>
2024-09-22 18:31:01 +0000

    Updating app.py

--- views/visualizar_horas.py
+++ views/visualizar_horas.py
@@ -3,7 +3,6 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import plotly.express as px
-import calplot
 import numpy as np
 
 st.set_page_config(page_title="Visualizar horas", page_icon="🕒", layout="wide")
@@ -16,10 +15,23 @@ def load_main_dataframe(worksheet):
 
 st.title("Controle de horas")
 
-df = load_main_dataframe("Horas por dia")
+# Carregando df das horas trabalhadas
+df_horas = load_main_dataframe("Horas por dia")
 
-df["Dia"] = pd.to_datetime(df["Dia"])
-df["period"] = df["Dia"].dt.to_period('M')
+df_horas["Dia"] = pd.to_datetime(df_horas["Dia"])
+df_horas["ano"] = df_horas["Dia"].dt.year
+
+df_horas["dia_da_semana"] = df_horas["Dia"].dt.day_name()
+df_horas["period"] = df_horas["Dia"].dt.to_period('M')
+
+# Criando df para o Heatmap
+dias_no_ano = pd.date_range(start=f'{df_horas["ano"].min()}-01-01', end=f'{df_horas["ano"].max()}-12-31', freq='D')
+
+heatmap_df = pd.DataFrame({
+    'Data': dias_no_ano,
+    'Horas trabalhadas': np.zeros(len(dias_no_ano))
+})
+
 
 filter = st.selectbox("Selecione o mês", df["period"].unique())
 
@@ -30,10 +42,3 @@ st.dataframe(filtered_df)
 bar_chart = px.bar(filtered_df, x='Dia', y='Horas trabalhadas')
 
 st.plotly_chart(bar_chart, use_container_width=True)
-
-all_days = pd.date_range('1/1/2019', periods=360, freq='D')
-days = np.random.choice(all_days, 500)
-events = pd.Series(np.random.randn(len(days)), index=days)
-teste = calplot.calplot(events, edgecolor=None, cmap='YlGn')
-
-st.plotly_chart(teste, use_container_width=True)
