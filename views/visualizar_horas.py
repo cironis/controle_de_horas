16f5932653186305d3ed538555e0859172b1217e
cironis <ciroshia@gmail.com>
2024-09-22 16:30:46 +0000

    Updating app.py

--- views/visualizar_horas.py
+++ views/visualizar_horas.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(page_title="Visualizar horas", page_icon="🕒", layout="wide")
 
@@ -18,4 +19,12 @@ df = load_main_dataframe("Horas por dia")
 df["Dia"] = pd.to_datetime(df["Dia"])
 df["period"] = df["Dia"].dt.to_period('M')
 
-st.dataframe(df)
+filter = st.selectbox("Selecione o mês", df["period"].unique())
+
+filtered_df = df.loc[df["period"] == filter]
+
+st.dataframe(filtered_df)
+
+bar_chart = px.bar(filtered_df, x='Dia', y='Horas trabalhadas')
+
+st.plotly_chart(bar_chart, use_container_width=True)
