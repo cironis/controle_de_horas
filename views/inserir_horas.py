d72aed3793eca2b169db0e3dbd92c3709b015ebc
cironis <ciroshia@gmail.com>
2024-10-05 16:31:03 +0000

    Updating app.py

--- views/inserir_horas.py
+++ views/inserir_horas.py
@@ -55,17 +55,21 @@ def inserir_horas(date_picker,number_input):
 
     df_horas = pd.concat([df_horas, novo_registro], ignore_index=True)
     st.session_state['horas_df'] = df_horas
+
     update_dataframe("Horas por dia",df_horas)
     st.session_state['clicked'] = True
     st.balloons()
 
     return True
 
+if st.query_params["auto_login"] == "vai_vamo_logo":
+    st.session_state['authentication_status'] = True
+
 if st.session_state['authentication_status']:
 
     authenticator.logout("Logout", "sidebar")
     st.write(f'Inserir Horas')
-    date_picker = st.date_input("When's your birthday", datetime.today(),format="DD/MM/YYYY")
+    date_picker = st.date_input("Data", datetime.today(),format="DD/MM/YYYY")
     number_input = st.number_input("Horas para inserir", min_value=0.0, step=0.5)
     button_return = st.button('Inserir Horas', on_click=inserir_horas, args=(date_picker,number_input))
 
@@ -76,3 +80,6 @@ elif st.session_state['authentication_status'] is False:
     st.error('Username/password is incorrect')
 elif st.session_state['authentication_status'] is None:
     st.warning('Please enter your username and password')
+
+
+st.write(st.query_params)
