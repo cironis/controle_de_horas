a9ab5029cd383c13e8bdde9f0ba231edc1631f81
cironis <ciroshia@gmail.com>
2024-10-05 16:08:28 +0000

    Updating app.py

--- views/inserir_horas.py
+++ views/inserir_horas.py
@@ -34,11 +34,28 @@ def update_dataframe(worksheet,df):
 
     return
 
+def inserir_horas(date_picker,number_input):
+    df_horas = load_main_dataframe("Horas por dia")
+    df_horas["Dia"] = pd.to_datetime(df_horas["Dia"])
+    novo_registro = pd.DataFrame({
+        "Dia": [date_picker],
+        "Horas trabalhadas": [number_input]
+    })
+    df_horas = pd.concat([df_horas, novo_registro], ignore_index=True)
+    update_dataframe("Horas por dia",df_horas)
+    st.success("Horas inseridas com sucesso!")
+    st.balloons()
+    return 
+
 if st.session_state['authentication_status']:
+
     authenticator.logout("Logout", "sidebar")
     st.write(f'Inserir Horas')
     date_picker = st.date_input("When's your birthday", datetime.today())
     number_input = st.number_input("Horas para inserir", min_value=0.0, step=0.5)
+    st.button('Inserir Horas', on_click=inserir_horas, args=(date_picker,number_input))
+
+
 
 elif st.session_state['authentication_status'] is False:
     st.error('Username/password is incorrect')
